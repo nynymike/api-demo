ddb86a167a59d6796a2a217dfe91dca752316d98
Michael Schwartz <mike@system76-pc.localdomain>
2020-04-02 10:41:32 -0500

    Renamed stuff

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp gg-test.py /var/www/html/api-demo
-cp certs.pem /var/www/html/api-demo
-
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 export REQUESTS_CA_BUNDLE=/var/www/html/api-demo/certs.pem
-/usr/bin/python3 /var/www/html/api-demo/gg-test.py
-
+/usr/bin/python3 gg-web.py
